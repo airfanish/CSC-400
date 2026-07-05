c1dd481e76b80994fa8d1b81c29417f1b54d7c4e
airfanish <aishah.irfan@csuglobal.edu>
2026-07-05 14:25:32 -0500

    Update Bag-1.java

--- Bag-1.java
+++ Bag-1.java
@@ -2,32 +2,60 @@ package criticalThinking;
 
 import java.util.ArrayList;
 
+/**
+ * A generic Bag class that stores objects using an ArrayList.
+ * Supports adding, removing, counting, merging, and retrieving distinct items.
+ */
 public class Bag<T> {
 
 	//Initialize ArrayList
 	private ArrayList<T> bagItems;
 	
-	//Constructor to create the object
+	/**
+ 	* Constructs an empty Bag.
+ 	*/
 	public Bag() {
 		bagItems = new ArrayList<>();
 	}
-	
-	//add items to the bag
+
+	/**
+ 	* Adds an item to the bag if it is not null.
+ 	*
+ 	* @param item the item to add to the bag
+ 	*/
 	public void add(T item) {
-		bagItems.add(item);
+		if (item != null) {
+			bagItems.add(item);
 	}
+		else {
+			System.out.println("Null values cannot be added to the bag.");
+		}
 	
-	//remove items from the bag
+	/**
+ 	* Removes an item from the bag.
+ 	*
+ 	* @param item the item to remove
+ 	*/
 	public void remove(T item) {
 		bagItems.remove(item);
 	}
 	
-	//check whether bag contains items
+	/**
+ 	* Checks whether the bag contains the specified item.
+ 	*
+ 	* @param item the item to search for
+ 	* @return true if the item is found; otherwise false
+ 	*/
 	public boolean contains(T item) {
 		return bagItems.contains(item);
 	}
 	
-	//implement count
+	/**
+ 	* Counts the number of occurrences of an item in the bag.
+ 	*
+ 	* @param item the item to count
+ 	* @return the number of times the item appears
+ 	*/
 	public int count(T item) {
 		int count = 0;
 		
@@ -40,12 +68,20 @@ public class Bag<T> {
 		
 	}
 	
-	//return the total number of items in the bag
+	/**
+ 	* Returns the total number of items in the bag.
+ 	*
+ 	* @return the size of the bag
+ 	*/
 	public int size() {
 		return bagItems.size();
 	}
 	
-	//merge the bags
+	/**
+ 	* Merges another bag into this bag.
+ 	*
+ 	* @param otherBag the bag to merge
+ 	*/
 	public void merge(Bag<T> otherBag) {
 		int i = 0;
 		
@@ -54,7 +90,11 @@ public class Bag<T> {
 		}
 	}
 	
-	//create a distinct method
+	/**
+ 	* Creates a new bag containing only distinct items.
+ 	*
+ 	* @return a bag containing unique items
+ 	*/
 	public Bag<T> distinct() {
 		//create distinctBag
 		Bag<T> distinctBag = new Bag<>();
@@ -69,7 +109,11 @@ public class Bag<T> {
 		return distinctBag;
 	}
 	
-	//print the bag contents
+	/**
+ 	* Returns the contents of the bag as a string.
+ 	*
+ 	* @return the bag contents
+ 	*/
 	public String toString() {
 		return bagItems.toString();
 	}
